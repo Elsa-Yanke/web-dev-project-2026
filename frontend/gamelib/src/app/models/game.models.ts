489748032f691e05afe9c920b94685b58cc1e02e
madinase <madinase12@gmail.com>
2026-04-16 15:43:46 +0500

    Create game.models.ts
    add Game interface

---
+++ frontend/gamelib/src/app/models/game.models.ts
@@ -0,0 +1,10 @@
+export interface Game {
+    id: number;
+    title: string;
+    description: string;
+    release_year: number;
+    price: string;
+    genre: number;
+    ai_summary: string;
+}
+
